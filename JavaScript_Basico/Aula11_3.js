eada80693e5df39259e085e8ca109c12def76ade
Lucas de Souza santos <lucass.santoss210@gmail.com>
2022-04-08 13:36:39 -0300

    estrutura de repetição aula 11

---
+++ JavaScript_Basico/Aula11_3.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < 5; i++)
+{
+    console.log(i);
+}
+
+let j = 0;
+for( ; ; )
+{
+    if(j < 6) console.log(j);
+    else break;
+    j++;
+}
+
+// número impa entre 300 e 333
+for(let i = 301; i <= 311; i += 2)
+{
+    console.log(i);
+}
